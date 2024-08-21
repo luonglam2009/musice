ad94f8bbda097235bbca9d795ee036477b1e76bd
luonglam2009 <141635635+luonglam2009@users.noreply.github.com>
2024-08-22 06:42:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,14 +6,6 @@ document.getElementById('playButton').addEventListener('click', function() {
         audio.pause();
     }
 });
-document.getElementById('playButton').addEventListener('click', function() {
-    var audio = document.getElementById('audioPlayer');
-    if (audio.paused) {
-        audio.play();
-    } else {
-        audio.pause();
-    }
-});
 
 document.getElementById('volumeControl').addEventListener('input', function() {
     var audio = document.getElementById('audioPlayer');
